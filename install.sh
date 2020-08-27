e9185cebb5c0fcf5a3da42db336f89c2bdb81d6a
Naratsunara <70288796+Naratsunara@users.noreply.github.com>
2020-08-28 03:40:29 +0700

    Update and rename fbrute.py to install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+apt update -y
+apt upgrade -y
+
+apt install iputils-ping inetutils-ping telnet telnetd etutils-telnet inetutils-telnetd ssh nmap net-tools inetutils-tools moreutils wireless-tools zsh git wget curl nano bash -y
+
+apt install python python-pip python-dev python-doc python2 python2-dev python2-doc python2.7 python2.7-dev python2.7-doc python3 python3-pip python3.8 python3.8-dev python3.8-doc python3.9 python3.9-dbg python3.9-dev python3.9-doc -y
+apt install php php7.4 phpmyadmin -y
+apt install ruby ruby2.7 ruby2.7-dev ruby2.7-doc -y
+apt install nodejs nodejs-doc node-bash -y
+
+apt install postgresql -y
+apt install apache2 -y
+apt install nginx -y
+
+apt install nmap -y
+apt install hydra -y
+apt install aircrack-ng -y
+apt install reaver -y
+apt install cupp -y
+apt install sqlmap -y
+apt install websploit -y
+apt install pompem -y
+apt install dirbuster -y
+apt install dir -y
+apt install teamsploit
+apt install set -y
+apt install metasploit-framework -y
+
+apt update -y
+apt upgrade -y
